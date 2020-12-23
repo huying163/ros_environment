dce854fe5d9397c8e09c4889a62e6f3b41fce1e2
huying163 <huying0101154869@163.com>
2020-12-23 16:44:16 +0800

    Update main.cpp

--- 1_perception_cv/detect_and_track/src/detection/main.cpp
+++ 1_perception_cv/detect_and_track/src/detection/main.cpp
@@ -7,12 +7,16 @@
   * @date 2018-04-02
  */
 #include <sstream>
+/*c++ standard input library*/
 #include <iostream>
 #include <time.h>
+/*opencv2 standard head file*/
 #include <opencv2/opencv.hpp>
 #include <chrono>
 #include <cstring>
+/*ros standard head file*/
 #include "ros/ros.h"
+/*coding function for ros image types*/
 #include "std_msgs/String.h"
 #include "defines.hpp"
 #include "ConcurrentQueue.hpp"
@@ -33,6 +37,7 @@ Settings settings("settings.xml");
 
 } // namespace detectionNodeShared
 
+// main function
 int main(int argc, char **argv)
 {
     detectionNodeShared::rosIntertface = new ROSInterface(argc, argv);
